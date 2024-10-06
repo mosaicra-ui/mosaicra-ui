753e8ec4702424c9ff5a3f4b770e17f36e3a65bc
IngaleChinmay04 <143017442+IngaleChinmay04@users.noreply.github.com>
2024-10-06 18:22:35 +0530

    Updated Audio Component

--- Media/Audio Component/AudioComponent.js
+++ Media/Audio Component/AudioComponent.js
@@ -2,7 +2,9 @@ const audio = document.getElementById("audio");
 const playPauseAudioButton = document.getElementById("playPauseAudio");
 const audioPlayPauseIcon = document.getElementById("audioPlayPauseIcon");
 const audioVolumeSlider = document.getElementById("audioVolume");
+const audioPlaybackRate = document.getElementById("audioPlaybackRate");
 const audioProgressSlider = document.getElementById("audioProgress");
+const beatBars = document.querySelectorAll(".beat-bar");
 
 // Play/Pause functionality
 playPauseAudioButton.addEventListener("click", () => {
@@ -10,10 +12,12 @@ playPauseAudioButton.addEventListener("click", () => {
     audio.play();
     audioPlayPauseIcon.classList.remove("fa-play");
     audioPlayPauseIcon.classList.add("fa-pause");
+    startBeatAnimation();
   } else {
     audio.pause();
     audioPlayPauseIcon.classList.remove("fa-pause");
     audioPlayPauseIcon.classList.add("fa-play");
+    stopBeatAnimation();
   }
 });
 
@@ -33,3 +37,21 @@ audioProgressSlider.addEventListener("input", (e) => {
 audioVolumeSlider.addEventListener("input", (e) => {
   audio.volume = e.target.value;
 });
+
+// Playback speed control
+audioPlaybackRate.addEventListener("change", (e) => {
+  audio.playbackRate = e.target.value;
+});
+
+// Beat animation control
+function startBeatAnimation() {
+  beatBars.forEach((bar) => {
+    bar.classList.add("beat-bar");
+  });
+}
+
+function stopBeatAnimation() {
+  beatBars.forEach((bar) => {
+    bar.classList.remove("beat-bar");
+  });
+}
